9d8b86a7e2168b8a4d4148ccf85f487151d61fa2
PlasmaShadow <plasmashadowx@gmail.com>
2017-05-12 09:52:07 +0000

    Remove few indentations

--- app/client/components/index.js
+++ app/client/components/index.js
@@ -21,7 +21,7 @@ class App extends Component{
     }
 
     render(){
-        return (<div style={{width: 100}}>
+        return (<div>
             <Layout fixedHeader>
                 <Header title="DEVMAP">
                    <Navigation>
